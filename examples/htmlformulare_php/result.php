3cff1393f10d023d9cc6f468fdbefb30ac214b30
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-01-24 14:01:58 +0100

    moved comments from result.php to index.html

--- examples/htmlformulare_php/result.php
+++ examples/htmlformulare_php/result.php
@@ -9,16 +9,15 @@
 <a href="index.html">Go to index.html</a>
 <p>
     <?php
-    echo "Test this with different Browsers: Chrome, Firefox, ...; javascript enabled or disabled";
-    echo "<br><br>Type '&lt;script&gt;alert('Your are hacked')&lt;/script&gt;' into input field in index.html<br><br>";
-    // Uncomment next line to see impact of HTML entities
-    // echo "<br><br>Type <script>alert('Your are hacked')</script> into input field in index.html";
+    // case method="post"
     if (isset($_POST['myinput'])) {
         echo "<br>Eingabe POST: " . $_POST['myinput'];
     }
+    // case method="get"
     if (isset($_GET['myinput'])) {
         echo "<br>Eingabe GET: " . $_GET['myinput'];
     }
+    // method either "post" or "get"
     echo "<br>Eingabe REQUEST: " . $_REQUEST['myinput'];
     ?>
 </p>
