7acafa4bea8f9639a971e5bcad060eb3173c58c3
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-01-24 12:14:40 +0100

    added examples for type conversion in PHP

--- firstexercises/regex/moneycounter.php
+++ firstexercises/regex/moneycounter.php
@@ -2,17 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Money Counter</title>
+    <title><!-- TODO: Place a title here --></title>
 </head>
 <body>
 <?php
-// TODO: Get the data from the form in index.html
 
-// TODO: Create a regular expression that matches the required criteria
+// TODO: see examples/regex for an example, how to implent this exercise
 
-// TODO: Match amounts of money using the regex and sum up the result
+// TODO: Declare and initialize a variable, that holds the sum of EUROs
 
-// TODO: Display the resulting amount
+// TODO: Get the data from the form in index.html. Check if the given key exists in $_POST (isset())
+
+// TODO: Create a regular expression that matches the required criteria using preg_match_all (see PHP Documentation)
+
+// TODO: Loop with foreach or in a similar way over matching amounts of money in the result of preg_match_all()
+// TODO: Use var_dump() to see, what it looks like.
+
+// TODO: use str_replace() to change commas "," to decimal points "." to be able to sum up the money in PHP
+
+// TODO: depending on your regular expression, you may need to replace " EUR" with "" to get plain numbers.
+
+// TODO: OPTIONAL: You can do both steps at once with preg_replace_callback_array() (see PHP Documentation)
+
+// TODO: Display the text used for testing and the resulting amount (echo).
+
+// TODO: Use number_format() for formatting the sum. Use htmlentities() or a similar function to avoid xss.
 ?>
+    <a href="index.html">Start Over</a>
 </body>
 </html>
