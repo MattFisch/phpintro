a4e99fce2594ccbe4815e83454bf5e40aa992a4d
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-02-12 10:28:20 +0100

    adding oophp exercise

---
+++ src/exercises/oo-php/oophp.php
@@ -0,0 +1,95 @@
+<?php
+namespace phpintro\src\exercises\oophp;
+
+/**
+ * Einbinden des Errorhandlings, damit Sie Fehler im Browser angezeigt bekommen.
+ * Erleichterung des Debugging
+ */
+require_once 'error_handling.php';
+/**
+ * TODO Sehen Sie sich die Bespiele in examples/oophp an
+ * TODO Zu Beginn DefineAndConst.php und Inheritance.php
+ * TODO danach auch Methods.php. (Vor allem wenn sie vollständig PSR2 konform werden möchten. Nicht Teil dieser UE)
+ *
+ * TODO Vergeben sie einen Klassennamen passend zu PSR1 in StudlyCaps
+ */
+
+
+    /**
+     * TODO definieren sie eine Klassenkonstante
+     * TODO siehe examples/oophp/DefindeAndConst.php
+     */
+
+
+    /**
+     * TODO definieren sie eine Klasseneigenschaft mit Scope public
+     * TODO siehe examples/oophp/PublicProtectedPrivateProperties.php
+     */
+
+
+    /**
+     * TODO Definieren sie einen Konstruktor
+     * TODO siehe examples/oophp/DefindeAndConst.php
+     */
+
+
+        // TODO Geben sie die oben definierte Klassenkonstante hier aus.
+        // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
+        // todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+
+
+        // TODO Geben sie die oben definierte Klasseneigenschaft hier aus.
+        // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
+        // todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+
+
+        // TODO Geben Sie den Wert der globalen Konstante DEBUG aus error_handling.php hier aus
+        // TODO Verfahren sie dazu wie bei der Klassenkonstante
+
+
+    /**
+     * TODO Definieren Sie Methode mit Scope public
+     * TODO siehe examples/oophp/Inheritance.php
+     * TODO Geben Sie in dieser Methode den Inhalt des $_POST-Arrays das von indes.html geschickt wird aus
+     * TODO Verwenden sie echo und testen Sie XSS in Chrome und Firefox: <script>alert('hacked')</script>
+     * TODO In einem zweiten Schritt verhindern Sie XSS für alle Browser
+     */
+
+
+
+/*
+ * TODO Erzeugen Sie das Objekt
+ * TODO Benennen sie das Objekt passend zur Klasse und zu PSR1 in camelCase.
+ */
+
+
+// TODO Rufen Sie die Methode mit Scope public hier auf
+
+
+// TODO Geben sie das Objekt mit var_dump() aus
+
+
+// TODO Geben sie die oben definierte Klassenkonstante im Konstruktor aus.
+// TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
+// todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+
+
+/*
+ * TODO Erzeugen Sie ein zweites Objekt mit unterschiedlichem Namen
+ * TODO Benennen sie das Objekt passend zur Klasse und zu PSR1 in camelCase.
+ */
+
+
+// TODO Geben sie das Objekt mir var_dump() aus
+
+
+// TODO Geben sie die oben definierte Klasseneigenschaft hier aus.
+// TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
+// todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+
+
+// TODO Geben Sie den Wert der globalen Konstante DEBUG aus error_handling.php hier aus
+// TODO Verfahren sie dazu wie bei der Klassenkonstante
+
+
+// TODO Geben Sie nur einen Zeilenumbruch am Ende an und kein closing Tag für PHP gemäß PSR2
