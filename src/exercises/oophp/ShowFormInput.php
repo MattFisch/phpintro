d258d8441cc212a3635d43ad15e24d3ea4947bf5
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-03-15 13:44:00 +0100

    improved TODOs

--- src/exercises/oophp/ShowFormInput.php
+++ src/exercises/oophp/ShowFormInput.php
@@ -11,9 +11,15 @@ require_once 'error_handling.php';
  * TODO Zu Beginn DefineAndConst.php und Inheritance.php
  * TODO danach auch Methods.php. (Vor allem wenn sie vollständig PSR2 konform werden möchten. Nicht Teil dieser UE)
  *
- * TODO Vergeben sie einen Klassennamen passend zu PSR1 in StudlyCaps
+ * TODO Definieren Sie ein Klasse mit einem Klassennamen passend zu PSR1 in StudlyCaps
+ * Diese Klasse soll nicht mehr können, als den Inhalt von $_POST aus index.html ausgeben
+ * Zusätzlich sollen noch Klassenkonstanten und Eigenschaften der Klasse ausgegeben werden
+ * In einer Methode wird der Inhalt von $_POST verarbeitet und diese Methode dann aufgerufen
  */
 
+
+
+    //TODO innerhalb dieser ersten Klasse
 
     /**
      * TODO definieren sie eine Klassenkonstante
@@ -33,63 +39,71 @@ require_once 'error_handling.php';
      */
 
 
+        // TODO Im Konstrutor
         // TODO Geben sie die oben definierte Klassenkonstante hier aus.
         // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
         // todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
-
 
         // TODO Geben sie die oben definierte Klasseneigenschaft hier aus.
         // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
         // todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
 
-
         // TODO Geben Sie den Wert der globalen Konstante DEBUG aus error_handling.php hier aus
         // TODO Verfahren sie dazu wie bei der Klassenkonstante
+
+        // TODO Ende Konstruktor
 
 
     /**
      * TODO Definieren Sie eine Methode mit Scope public
      * TODO siehe examples/oophp/Inheritance.php
-     * TODO Geben Sie in dieser Methode den Inhalt des $_POST-Arrays das von indes.html geschickt wird aus
-     * TODO Verwenden sie echo und testen Sie XSS in Chrome und Firefox: <script>alert('hacked')</script>
-     * TODO In einem zweiten Schritt verhindern Sie XSS für alle Browser
      */
 
+         // TODO Innerhalb der Methode
+
+         // TODO Geben Sie in dieser Methode den Inhalt des $_POST-Arrays das von index.html geschickt wird aus
+         // TODO Verwenden sie echo und testen Sie XSS in Chrome und Firefox: <script>alert('hacked')</script>
+         // TODO In einem zweiten Schritt verhindern Sie XSS für alle Browser
 
 
-/*
- * TODO Erzeugen Sie das Objekt
- * TODO Benennen sie das Objekt passend zur Klasse und zu PSR1 in camelCase.
- */
 
 
-// TODO Rufen Sie die Methode mit Scope public hier auf
+         // TODO Ende der Methode
+
+// TODO Ende der Klasse
 
 
-// TODO Geben sie das Objekt mit var_dump() aus
+// TODO Erzeugen Sie ein erstes Objekt der Klasse
+// TODO Benennen sie das Objekt passend zur Klasse und zu PSR1 in camelCase.
 
 
-// TODO Geben sie die oben definierte Klassenkonstante im Konstruktor aus.
+// TODO Rufen Sie die Methode der Klasse mit Scope public hier aus
+
+
+// TODO Geben sie das erste Objekt mit var_dump() aus
+
+
+// TODO Geben sie die oben definierte Klassenkonstante hier aus.
 // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
-// todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+// TODO Umgeben sie dazu den Wert mit einem <p> oder einem anderem Tag
 
 
-/*
- * TODO Erzeugen Sie ein zweites Objekt mit unterschiedlichem Namen
- * TODO Benennen sie das Objekt passend zur Klasse und zu PSR1 in camelCase.
- */
+
+// TODO Erzeugen Sie ein zweites Objekt mit unterschiedlichem Namen
+// TODO Benennen sie das zweite Objekt passend zur Klasse und zu PSR1 in camelCase.
 
 
-// TODO Geben sie das Objekt mit var_dump() aus
+// TODO Geben sie das zweite Objekt mit var_dump() aus
 
 
 // TODO Geben sie die oben definierte Klasseneigenschaft hier aus.
 // TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
-// todo Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
+// TODO Umgeben sie Dazu den Wert mit einem <p> oder einem anderem Tag
 
 
 // TODO Geben Sie den Wert der globalen Konstante DEBUG aus error_handling.php hier aus
-// TODO Verfahren sie dazu wie bei der Klassenkonstante
+// TODO Gestalten sie die Ausgabe so, dass dabei valides HTML entsteht
+// TODO Umgeben sie dazu den Wert mit einem <p> oder einem anderem Tag
 
 
 // TODO Geben Sie nur einen Zeilenumbruch am Ende an und kein closing Tag für PHP gemäß PSR2
