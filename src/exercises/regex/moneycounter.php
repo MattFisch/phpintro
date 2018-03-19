b6c15479e3e9233e711bb0f2dc31f7d1a9091987
Harrer Martin <P20137@hagenberg.fhooe.at>
2018-03-19 15:17:33 +0100

    improve TODOs and teststring for regex

--- src/exercises/regex/moneycounter.php
+++ src/exercises/regex/moneycounter.php
@@ -14,9 +14,14 @@
 // TODO: Get the data from the form in index.html. Check if the given key exists in $_POST (isset())
 
 // TODO: Create a regular expression that matches the required criteria using preg_match_all (see PHP Documentation)
+// TODO: complete the empty capturing group (...) of the regex "/((^[1-9]| [1-9]|(\R[1-9]))([0-9]*)| 0)(...)( EUR\h| EUR\b)/"
+// TODO: @see https://regex101.com/ for an explanation
+// TODO: Use the text snippet in teststring.txt to test your regex on http://www.phpliveregex.com/.
+// TODO: It shows all valid and invalid test cases and should sum up to 1000 EUR.
 
 // TODO: Loop with foreach or in a similar way over matching amounts of money in the result of preg_match_all()
-// TODO: Use var_dump() to see, what it looks like.
+// TODO: Or build a regex in a way, that you can use array_sum().
+// TODO: Use var_dump() to see, what $matches looks like (output of phpliveregex.com).
 
 // TODO: use str_replace() to change commas "," to decimal points "." to be able to sum up the money in PHP
 
@@ -26,9 +31,8 @@
 
 // TODO: Display the text used for testing and the resulting amount (echo).
 
-// TODO: Use number_format() for formatting the sum. Use htmlentities() or a similar function to avoid xss.
+// TODO: Use number_format() for formatting the sum. Use htmlentities() or a similar function to avoid XSS.
 
-// TODO: Use the text snippet in teststring.txt to test your regex. It shows all valid and invalid test cases and should sum up to 1000 EUR.
 
 ?>
     <a href="index.html">Start Over</a>
